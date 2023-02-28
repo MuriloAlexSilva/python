1d9bf15576ccd85a40914061d8a8a4a5bab0767d
Murilo Silva <murilo.alex.silva@gmail.com>
2023-02-27 23:32:29 -0300

    Iniciando em python

---
+++ fundamentos/tipagem/tipagem.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um numero: '))
+n2 = float(input('Digite outro numero: '))
+nome = str(input('Qual o seu nome: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}!'.format(n1, n2, soma))
+#Para obter o tipo da variavel
+print(type(nome))
+print(type(soma))
